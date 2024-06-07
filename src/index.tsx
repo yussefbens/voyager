74fa9cb31dae488505489c30b2cbe9f79aef7959
yussefbens <yussef.bens@gmail.com>
2024-06-07 19:04:15 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -26,7 +26,9 @@ import { getAndroidNavMode, isNative } from "./helpers/device";
 
     root.render(
       <React.StrictMode>
-        <App />
+        <Suspense fallback={<div>Loading...</div>}>
+          <App />
+        </Suspense>
       </React.StrictMode>,
     );
   }
